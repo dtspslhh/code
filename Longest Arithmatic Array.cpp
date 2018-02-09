31c7fb208986e7a190e30cf09a1a69ca07fd4ab6
Nate Liu <nate_liu@hotmail.com>
2018-02-08 16:40:56 -0800

    Add comment

--- Longest Arithmatic Array.cpp
+++ Longest Arithmatic Array.cpp
@@ -1,4 +1,11 @@
-int longestArithmaticArray(vector<int> nums){
+/*
+[1, 6, 3, 2, 5, 4, 7] return 4, because 1 3 5 7
+DP solution. dp stores an array of hash_map. Every hash map stores the current position number's every differentiation with prior ones longest path.
+
+
+*/
+
+int longestArithmeticArray(vector<int> nums){
 	int longest = 0, size = nums.size();
 	if(size < 3) return size;
 	vector<unordered_map<int,int>> dp(size);
@@ -6,6 +13,7 @@ int longestArithmaticArray(vector<int> nums){
 		for(int j = 0; j < i; j++){
 			int diff = nums[i] - nums[j];
 			dp[i][diff] = max(dp[i][diff], dp[j][diff] + 1);
+			//or dp[i][diff] = dp[j][diff] + 1; since later dp[j][diff] will be larger
 			longest = max(longest, dp[i][diff]);
 		}
 	}
